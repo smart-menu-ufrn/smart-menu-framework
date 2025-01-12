26eab32b98b573dee60bdfa9087fcba3275a9989
Rary Coringa <rary.goncalves.123@ufrn.edu.br>
2025-01-12 17:35:13 -0300

    add initial structure of framework with instances

---
+++ src/main/java/br/edu/ufrn/drinkstore/DrinkStoreApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufrn.drinkstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinkStoreApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/edu/ufrn/drugstore/DrugStoreApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufrn.drugstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrugStoreApplication.class, args);
+	}
+
+}
