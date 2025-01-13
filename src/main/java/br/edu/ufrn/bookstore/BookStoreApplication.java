e8e9a782cfa60c520f295bbfa353af21cf291a9f
Rary Coringa <rary.goncalves.123@ufrn.edu.br>
2025-01-13 11:00:45 -0300

    improve way to automatic scan of entities and repositories between packages

--- src/main/java/br/edu/ufrn/bookstore/BookStoreApplication.java
+++ src/main/java/br/edu/ufrn/bookstore/BookStoreApplication.java
@@ -2,9 +2,17 @@ package br.edu.ufrn.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(
 	scanBasePackages = {"br.edu.ufrn.bookstore", "br.edu.ufrn.smartmenu"}
+)
+@EntityScan(
+	basePackages = {"br.edu.ufrn.bookstore", "br.edu.ufrn.smartmenu"}
+)
+@EnableJpaRepositories(
+	basePackages = {"br.edu.ufrn.bookstore", "br.edu.ufrn.smartmenu"}
 )
 public class BookStoreApplication {
 
--- src/main/java/br/edu/ufrn/drugstore/DrugStoreApplication.java
+++ src/main/java/br/edu/ufrn/drugstore/DrugStoreApplication.java
@@ -2,9 +2,17 @@ package br.edu.ufrn.drugstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(
 	scanBasePackages = {"br.edu.ufrn.drugstore", "br.edu.ufrn.smartmenu"}
+)
+@EntityScan(
+	basePackages = {"br.edu.ufrn.drugstore", "br.edu.ufrn.smartmenu"}
+)
+@EnableJpaRepositories(
+	basePackages = {"br.edu.ufrn.drugstore", "br.edu.ufrn.smartmenu"}
 )
 public class DrugStoreApplication {
 
